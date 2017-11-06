6e35cafed7f18d6b393964d06e1521392d4d0342
aycrazy <andrewyaspan@gmail.com>
2017-11-06 15:17:21 -0600

    in case i didnt push conversion file changes

--- convert_to_json.py
+++ convert_to_json.py
@@ -4,7 +4,7 @@ import json
 csvfile = open('aptc.csv', 'r')
 jsonfile = open('small_aptc.json', 'w')
 
-fieldnames = ("county_fips_code","state_name","no_aptc","yes_aptc","total_plan_selections")
+fieldnames = ("county_fips_code","state_name", "county","no_aptc","yes_aptc","total_plan_selections")
 reader = csv.DictReader( csvfile, fieldnames)
 good_rows = []
 ct = 0
@@ -12,11 +12,12 @@ for idx,row in enumerate(reader):
 	if idx ==0:
 		continue
 	if ct < 30:
-		if row['yes_aptc'] not in ['NaN','.'] and row['total_plan_selections'] not in ['NaN','.']:
+		if row['yes_aptc'] not in ['NaN','.'] and row['total_plan_selections'] not in ['NaN','.']\
+		and row['county'] not in ['NaN','.'] :
 			print(row)
 			good_rows.append(row)
 
-	ct+=1
+			ct+=1
 
 out = json.dumps(good_rows)
 jsonfile.write(out)
